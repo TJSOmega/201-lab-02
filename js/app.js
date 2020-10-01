da3049aa960a1c0e2ac025080a571f63f8d1476a
Terrence Jamal Simms <Tjs@omegacc.com>
2020-09-30 22:03:35 -0700

    Updated lab files added lintr, js file, style sheet, and index file

--- js/app.js
+++ js/app.js
@@ -1,3 +1,77 @@
 'use strict';
 
-console.log('Hey hows it going?');
+
+// var name1 = prompt('What is your name?')
+// alert('Welcome to my About me page ' + name1 + '! Let\'s play a game!')
+
+// // Question 1
+// var que1 = prompt("Have I always worked in the tech industry?").toLowerCase()
+
+// console.log(que1)
+
+// if (que1 === 'yes' || que1 === 'y') {
+//   // console.log('Good job you were paying attention! Next Question!')
+//   alert('Good job you were paying attention! Next Question!')
+//   console.log()
+// }
+// else {
+//    // console.log('You are so wrong!')
+//   alert('You are so wrong!')
+// }
+
+// // Question 2
+// var que2 = prompt("Have I always worked in the tech industry?").toLowerCase()
+
+// console.log(que2)
+
+// if (que2 === 'yes' || que2 === 'y') {
+//   // console.log('Good job you were paying attention! Next Question!')
+//   alert('Good job you were paying attention! Next Question!')
+// }
+// else {
+//    // console.log('You are so wrong!')
+//   alert('You are so wrong!')
+// }
+
+// // Question 3
+// var que3 = prompt("Have I always worked in the tech industry?").toLowerCase()
+
+// console.log(que3)
+
+// if (que3 === 'yes' || que3 === 'y') {
+//   // console.log('Good job you were paying attention! Next Question!')
+//   alert('Good job you were paying attention! Next Question!')
+// }
+// else {
+//   // console.log('You are so wrong!')
+//   alert('You are so wrong!')
+// }
+
+// // Question 4
+// var que4 = prompt("Have I always worked in the tech industry?").toLowerCase()
+
+// console.log(que4)
+
+// if (que4 === 'yes' || que4 === 'y') {
+//   // console.log('Good job you were paying attention! Next Question!')
+//   alert('Good job you were paying attention! Next Question!')
+// }
+// else {
+//   // console.log('You are so wrong!')
+//   alert('You are so wrong!')
+// }
+
+// // Question 5
+// var que5 = prompt("Have I always worked in the tech industry?").toLowerCase()
+
+// console.log(que5)
+
+// if (que5 === 'yes' || que5 === 'y') {
+//   // console.log('Good job you were paying attention! Next Question!')
+//   alert('Good job you were paying attention! Next Question!')
+// }
+// else {
+//    // console.log('You are so wrong!')
+//   alert('You are so wrong!')
+// }
+
