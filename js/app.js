d04e0e137fd7d129325d09dbead7337afcf6fddf
Terrence Jamal Simms <Tjs@omegacc.com>
2020-10-01 19:52:05 -0700

    Send up of project files, should be complete.

--- js/app.js
+++ js/app.js
@@ -1,77 +1,92 @@
-'use strict';
+// 'use strict';
 
 
 // var name1 = prompt('What is your name?')
 // alert('Welcome to my About me page ' + name1 + '! Let\'s play a game!')
 
 // // Question 1
-// var que1 = prompt("Have I always worked in the tech industry?").toLowerCase()
+// var que1 = prompt("Was I raised in Texas?").toLowerCase()
 
 // console.log(que1)
 
 // if (que1 === 'yes' || que1 === 'y') {
-//   // console.log('Good job you were paying attention! Next Question!')
-//   alert('Good job you were paying attention! Next Question!')
+//   // console.log('Maaaannn got the first question wrong? Not looking good for you so far!')
+//   alert('Maaaannn got the first question wrong? Not looking good for you so far!')
 //   console.log()
 // }
-// else {
-//    // console.log('You are so wrong!')
-//   alert('You are so wrong!')
+// else if (que1 === 'no' || que1 === 'n'){
+//    // console.log('Got the first question right, you\'re off to a good start!')
+//   alert('Got the first question right, you\'re off to a good start!')
 // }
-
+// else {
+//   alert('Invalid Response')
+// }
 // // Question 2
 // var que2 = prompt("Have I always worked in the tech industry?").toLowerCase()
 
 // console.log(que2)
 
 // if (que2 === 'yes' || que2 === 'y') {
-//   // console.log('Good job you were paying attention! Next Question!')
-//   alert('Good job you were paying attention! Next Question!')
+//   // console.log('Good job! Next Question!')
+//   alert('Good job you! Next Question!')
 // }
-// else {
+// else if (que1 === 'no' || que1 === 'n') {
 //    // console.log('You are so wrong!')
 //   alert('You are so wrong!')
 // }
+// else {
+//   alert('Invalid Response')
+// }
 
 // // Question 3
-// var que3 = prompt("Have I always worked in the tech industry?").toLowerCase()
+// var que3 = prompt("I\'m a natural artist?").toLowerCase()
 
 // console.log(que3)
 
 // if (que3 === 'yes' || que3 === 'y') {
-//   // console.log('Good job you were paying attention! Next Question!')
-//   alert('Good job you were paying attention! Next Question!')
+//   // console.log('Sorry you chose the wrong answer better luck next question!')
+//   alert('Sorry you chose the wrong answer better luck next question!')
+// }
+// else if (que1 === 'no' || que1 === 'n') {
+//   // console.log('Got this question correct hotshot! Nice job!')
+//   alert('Got this question correct hotshot! Nice job!')
 // }
 // else {
-//   // console.log('You are so wrong!')
-//   alert('You are so wrong!')
+//   alert('Invalid Response')
 // }
 
 // // Question 4
-// var que4 = prompt("Have I always worked in the tech industry?").toLowerCase()
+// var que4 = prompt('I\'ve gotten most of my coding knowledge from youtube and skillshare? ').toLowerCase()
 
 // console.log(que4)
 
 // if (que4 === 'yes' || que4 === 'y') {
-//   // console.log('Good job you were paying attention! Next Question!')
-//   alert('Good job you were paying attention! Next Question!')
+//   // console.log('Wrooooonnnngggggg!')
+//   alert('Wrooooonnnngggggg!')
+// }
+// else if (que1 === 'no' || que1 === 'n') {
+//   // console.log('Good job!')
+//   alert('Good job!')
 // }
 // else {
-//   // console.log('You are so wrong!')
-//   alert('You are so wrong!')
+//   alert('Invalid Response')
 // }
 
 // // Question 5
-// var que5 = prompt("Have I always worked in the tech industry?").toLowerCase()
+// var que5 = prompt("My name is Terrence").toLowerCase()
 
 // console.log(que5)
 
 // if (que5 === 'yes' || que5 === 'y') {
-//   // console.log('Good job you were paying attention! Next Question!')
-//   alert('Good job you were paying attention! Next Question!')
+//   // console.log('Thought I\'d give you a free one! :)')
+//   alert('Thought I\'d give you a free one! :)')
+// }
+// else if (que1 === 'no' || que1 === 'n') {
+//    // console.log('If you got this wrong we can\'t be friends...')
+//   alert('If you got this wrong we can\'t be friends...')
+
 // }
 // else {
-//    // console.log('You are so wrong!')
-//   alert('You are so wrong!')
+//   alert('Invalid Response')
 // }
 
