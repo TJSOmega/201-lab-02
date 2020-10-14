e168be3e540061cdf2deb1e9e8eb2315ac02e750
Terrence Jamal Simms <Tjs@omegacc.com>
2020-10-13 19:56:53 -0700

    Made some CSS changes

--- js/app.js
+++ js/app.js
@@ -8,19 +8,19 @@ alert("Welcome to my About me page " + name1 + "! Let's play a game!");
 var pointcounter = 0;
 
 // Question 1
-function questionOne() {
+function questionOne(); {
   var que1 = prompt("Was I raised in Texas?").toLowerCase();
 
   console.log(que1);
 
   if (que1 === "yes" || que1 === "y") {
-    // console.log('Maaaannn got the first question wrong? Not looking good for you so far!')
+   
     alert(
       "Maaaannn got the first question wrong? Not looking good for you so far!"
     );
     console.log();
   } else if (que1 === "no" || que1 === "n") {
-    // console.log('Got the first question right, you\'re off to a good start!')
+    
     alert("Got the first question right, you're off to a good start!");
     pointcounter++;
   } else {
@@ -31,120 +31,120 @@ questionOne();
 
 // Question 2
 function questionTwo() {
-var que2 = prompt("Have I always worked in the tech industry?").toLowerCase();
+  var que2 = prompt("Have I always worked in the tech industry?").toLowerCase();
 
-console.log(que2);
+  console.log(que2);
 
-if (que2 === "yes" || que2 === "y") {
-  // console.log('Good job! Next Question!')
-  alert("Good job you! Next Question!");
-  pointcounter++;
-} else if (que1 === "no" || que1 === "n") {
-  // console.log('You are so wrong!')
-  alert("You are so wrong!");
-} else {
-  alert("Invalid Response");
-}
+  if (que2 === "yes" || que2 === "y") {
+    
+    alert("Good job you! Next Question!");
+    pointcounter++;
+  } else if (que1 === "no" || que1 === "n") {
+    
+    alert("You are so wrong!");
+  } else {
+    alert("Invalid Response");
+  }
 }
 questionTwo();
 
 // Question 3
 
-function questionThree(){
-var que3 = prompt("I'm a natural artist?").toLowerCase();
+function questionThree() {
+  var que3 = prompt("I'm a natural artist?").toLowerCase();
 
-console.log(que3);
+  console.log(que3);
 
-if (que3 === "yes" || que3 === "y") {
-  // console.log('Sorry you chose the wrong answer better luck next question!')
-  alert("Sorry you chose the wrong answer better luck next question!");
-} else if (que1 === "no" || que1 === "n") {
-  // console.log('Got this question correct hotshot! Nice job!')
-  alert("Got this question correct hotshot! Nice job!");
-  pointcounter++;
-} else {
-  alert("Invalid Response");
-}
+  if (que3 === "yes" || que3 === "y") {
+    
+    alert("Sorry you chose the wrong answer better luck next question!");
+  } else if (que1 === "no" || que1 === "n") {
+    
+    alert("Got this question correct hotshot! Nice job!");
+    pointcounter++;
+  } else {
+    alert("Invalid Response");
+  }
 }
 questionThree();
 
 // Question 4
 
-function questionFour(){
-var que4 = prompt(
-  "I've gotten most of my coding knowledge from youtube and skillshare? "
-).toLowerCase();
+function questionFour() {
+  var que4 = prompt(
+    "I've gotten most of my coding knowledge from youtube and skillshare? "
+  ).toLowerCase();
 
-console.log(que4);
+  console.log(que4);
 
-if (que4 === "yes" || que4 === "y") {
-  // console.log('Wrooooonnnngggggg!')
-  alert("Wrooooonnnngggggg!");
-} else if (que1 === "no" || que1 === "n") {
-  // console.log('Good job!')
-  alert("Good job!");
-  pointcounter++;
-} else {
-  alert("Invalid Response");
-}
+  if (que4 === "yes" || que4 === "y") {
+    
+    alert("Wrooooonnnngggggg!");
+  } else if (que1 === "no" || que1 === "n") {
+   
+    alert("Good job!");
+    pointcounter++;
+  } else {
+    alert("Invalid Response");
+  }
 }
 questionFour();
 
 // Question 5
 
-function questionFive(){
-var que5 = prompt("My name is Terrence").toLowerCase();
+function questionFive() {
+  var que5 = prompt("My name is Terrence").toLowerCase();
 
-console.log(que5);
+  console.log(que5);
 
-if (que5 === "yes" || que5 === "y") {
-  // console.log('Thought I\'d give you a free one! :)')
-  alert("Thought I'd give you a free one! :)");
-  pointcounter++;
-} else if (que1 === "no" || que1 === "n") {
-  // console.log('If you got this wrong we can\'t be friends...')
-  alert("If you got this wrong we can't be friends...");
-} else {
-  alert("Invalid Response");
-}
+  if (que5 === "yes" || que5 === "y") {
+    
+    alert("Thought I'd give you a free one! :)");
+    pointcounter++;
+  } else if (que1 === "no" || que1 === "n") {
+    
+    alert("If you got this wrong we can't be friends...");
+  } else {
+    alert("Invalid Response");
+  }
 }
 questionFive();
 
 // Template literal here, allows for you to use back ticks to create a string with auto concatenation
 alert(`Good job ${name1} time to play game #2!`);
 
-Random Number generator game 1-10, function and called by button on HTML page.
+// Random Number generator game 1-10, function and called by button on HTML page.
 
 function numberGame() {
 
-var tries1 = 0;
-var rannum1 = Math.floor(Math.random() * 10 + 1);
-console.log(rannum1);
-var guess1;
+  var tries1 = 0;
+  var rannum1 = Math.floor(Math.random() * 10 + 1);
+  console.log(rannum1);
+  var guess1;
 
-while (tries1 <= 3 && guess1 != rannum1) {
-  guess1 = parseInt(prompt("Alright " + name1 + " guess a number 1-10"));
+  while (tries1 <= 3 && guess1 != rannum1) {
+    guess1 = parseInt(prompt("Alright " + name1 + " guess a number 1-10"));
 
-  if (guess1 < rannum1) {
-    alert("Your guess was to low!");
-    tries1++;
-    // console.log('tries amount ' + tries)
-  } else if (guess1 > rannum1) {
-    alert("Your guess was to high!");
-    tries1++;
-    // console.log('tries amount ' + tries)
+    if (guess1 < rannum1) {
+      alert("Your guess was to low!");
+      tries1++;
+      
+    } else if (guess1 > rannum1) {
+      alert("Your guess was to high!");
+      tries1++;
+      
+    }
   }
-}
-if (rannum1 === guess1) {
-  alert("You Win!!!");
-  pointcounter++;
-} else {
-  alert("You Lose...");
-}
+  if (rannum1 === guess1) {
+    alert("You Win!!!");
+    pointcounter++;
+  } else {
+    alert("You Lose...");
+  }
 }
 numberGame();
 
-// console.log(guess1);
+
 
 
 
@@ -153,28 +153,28 @@ numberGame();
 // var 7th Question
 
 function movieGuessing() {
-var correctAnswers = ["horror", "comedy", "romance", "action", "drama"];
-var guessAnswer = prompt("What types of movies do you think?");
-var attemptsRemaining = 6;
-var answeredCorrectly = false;
-while (attemptsRemaining > 0 && !answeredCorrectly) {
-  attemptsRemaining--;
-  for (var i = 0; i < correctAnswers.length; i++) {
-    if (guessAnswer === correctAnswers[i]) {
-      answeredCorrectly = true;
-      alert(`Yeah! ${guessAnswer} is one of the types of movies I watch!`);
-      pointcounter++;
+  var correctAnswers = ["horror", "comedy", "romance", "action", "drama"];
+  var guessAnswer = prompt("What types of movies do you think?");
+  var attemptsRemaining = 6;
+  var answeredCorrectly = false;
+  while (attemptsRemaining > 0 && !answeredCorrectly) {
+    attemptsRemaining--;
+    for (var i = 0; i < correctAnswers.length; i++) {
+      if (guessAnswer === correctAnswers[i]) {
+        answeredCorrectly = true;
+        alert(`Yeah! ${guessAnswer} is one of the types of movies I watch!`);
+        pointcounter++;
+      }
+    }
+    if (attemptsRemaining > 0 && !answeredCorrectly) {
+      guessAnswer = prompt(
+        "Sorry that wasn't a correct answer please try again!"
+      );
+    }
+    if (attemptsRemaining === 0 && !answeredCorrectly) {
+      alert("Sorry you lose!");
     }
   }
-  if (attemptsRemaining > 0 && !answeredCorrectly) {
-    guessAnswer = prompt(
-      "Sorry that wasn't a correct answer please try again!"
-    );
-  }
-  if (attemptsRemaining === 0 && !answeredCorrectly) {
-    alert("Sorry you lose!");
-  }
-}
 }
 movieGuessing();
 alert(`You got ${pointcounter} questions correct!`);
